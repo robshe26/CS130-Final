ca6e4dda880f2595c55e3305d79a37a5f653f3f5
Robert Sheehan <sheehare23@bonaventure.edu>
2026-04-07 22:44:44 -0400

    Calculate average city MPG from CSV data

--- Cars.py
+++ Cars.py
@@ -1,3 +1,22 @@
+import csv
 
-  
+total_city_mpg = 0
+total_cars = 0
+
+with open('cars (1).csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    next(reader)  
+
+
+    for row in reader:
+        city_mpg_str = row[8]
+        
+        city_mpg = float(city_mpg_str)
+
+        total_city_mpg += city_mpg
+        total_cars += 1
+
+    average_city_mpg = total_city_mpg / total_cars
+
+    print("Average City MPG:", round(average_city_mpg, 2))
   
